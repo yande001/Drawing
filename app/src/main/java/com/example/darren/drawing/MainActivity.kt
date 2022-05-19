2b7671877ce54af21670ace59141e9bc0aed048d
Darren <sengerby300@gmail.com>
2022-05-19 19:27:54 +0800

    select brush color

--- app/src/main/java/com/example/darren/drawing/MainActivity.kt
+++ app/src/main/java/com/example/darren/drawing/MainActivity.kt
@@ -3,14 +3,27 @@ package com.example.darren.drawing
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayoutPaintColor = findViewById<LinearLayout>(R.id.ll_paint_color)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColor[2] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+        )
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setBrushSize(20.0f)
@@ -41,5 +54,22 @@ class MainActivity : AppCompatActivity() {
             brushDialog.dismiss()
         }
         brushDialog.show()
+    }
+
+    fun paintClicked(view: View){
+        if(view != mImageButtonCurrentPaint){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+            )
+            mImageButtonCurrentPaint!!.setImageDrawable(
+                ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 }
