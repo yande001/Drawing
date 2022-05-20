f11f0d0fad757883d1aa3c1d523064070239f23f
Darren <sengerby300@gmail.com>
2022-05-20 19:25:14 +0800

    share photo

--- app/src/main/java/com/example/darren/drawing/MainActivity.kt
+++ app/src/main/java/com/example/darren/drawing/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -209,6 +210,7 @@ class MainActivity : AppCompatActivity() {
                             Toast.makeText(this@MainActivity,
                                 "File saved successfully: $result",
                                 Toast.LENGTH_LONG).show()
+                                shareImage(result)
                         } else{
                             Toast.makeText(this@MainActivity,
                                 "Something went wrong while saving the file.",
@@ -239,6 +241,17 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
         }
     }
 }
